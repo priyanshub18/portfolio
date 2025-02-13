6367ca3213eeb999b5bd0407629dc4a20daea717
priyanshub18 <priyanshubhardwaj658@gmail.com>
2025-02-13 10:44:51 +0530

    added-font-inter-and-calistoga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter, Calistoga } from 'next/font/google';
+import './globals.css';
+import { twMerge } from 'tailwind-merge';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+const calistoga = Calistoga({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight: ['400'],
+});
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: 'My Portfolio',
+  description: 'Created with the help of Frontend Tribe',
 };
 
 export default function RootLayout({
@@ -12,8 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          'bg-gray-900 text-white antialiased font-sans',
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
