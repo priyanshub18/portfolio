226e0ffe91f2a5fb7da6496f3967990ff3776b96
priyanshub18 <priyanshubhardwaj658@gmail.com>
2025-02-16 23:52:12 +0530

    final-about-section

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -12,7 +12,9 @@ function CardHeader({
 }) {
   return (
     <div>
-      <div className={twMerge('flex flex-col', className)}>
+      <div
+        className={twMerge('flex p-6 flex-col md:py-8 md:px-10 ', className)}
+      >
         <div className='inline-flex gap-2'>
           <StarIcon className='size-9 text-emerald-300' />
           <h3 className='font-serif text-3xl'>{title}</h3>
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -46,12 +46,12 @@ const hobbies = [
   {
     title: 'Traveling',
     emoji: '✈️',
-    left: '5%',
+    left: '2.5%',
     top: '5%',
   },
   { left: '50%', top: '5%', title: 'Reading', emoji: '🔖' },
   { left: '10%', top: '35%', title: 'Photography', emoji: '📸' },
-  { left: '35%', top: '40%', title: 'Gaming', emoji: '🎮' },
+  // { left: '35%', top: '40%', title: 'Gaming', emoji: '🎮' },
   {
     title: 'Movies',
     emoji: '🎬',
@@ -68,91 +68,94 @@ const hobbies = [
 ];
 export const AboutSection = () => {
   return (
-    <div className='py-20'>
-      <div className='container'>
+    <div className='py-20 lg:py-28'>
+      <div className='container '>
         <SectionHeader
           eyebrow='About Me'
           title='A Glimpse into My Word'
           description='Learn more about who I am , what I do and what inspires me.'
         />
 
-        <div className='mt-20 flex flex-col gap-6'>
-          <Card className='h-[320px]'>
-            <CardHeader
-              title='My Reads'
-              description='Explore the books shaping my perspectives.'
-            />
-            <div className='w-40 mx-auto mt-8'>
-              <Image
-                src={bookImage}
-                alt='Book Cover'
-                width={500}
-                height={500}
+        <div className='mt-20 flex flex-col gap-8'>
+          <div className='grid md:grid-cols-5 md:gap-8 sm:grid-cols-1 gap-8 lg:grid-cols-3'>
+            <Card className='h-[320px] md:col-span-2 lg:col-span-1'>
+              <CardHeader
+                title='My Reads'
+                description='Explore the books shaping my perspectives.'
               />
-            </div>
-          </Card>
+              <div className='w-40 mx-auto mt-2 md:mt-0'>
+                <Image
+                  src={bookImage}
+                  alt='Book Cover'
+                  width={500}
+                  height={500}
+                />
+              </div>
+            </Card>
 
-          <Card className='h-[320px] p-0'>
-            <CardHeader
-              className='px-6 pt-6'
-              title='My Toolbox'
-              description='Explore the technologies and tools I use to create innovative solutions.'
-            />
+            <Card className='h-[320px] md:col-span-3 lg:col-span-2'>
+              <CardHeader
+                // className='px-6 pt-6'
+                title='My Toolbox'
+                description='Explore the technologies and tools I use to create innovative solutions.'
+              />
 
-            <ToolItems toolboxItems={toolboxItems} className='mt-6' />
-            <ToolItems
-              toolboxItems={toolboxItems}
-              className='mt-6 '
-              itemsWrapperclassName='-translate-x-1/2
+              <ToolItems toolboxItems={toolboxItems} className='mt-6' />
+              <ToolItems
+                toolboxItems={toolboxItems}
+                className='mt-6 '
+                itemsWrapperclassName='-translate-x-1/2
               1/2'
-            />
-          </Card>
-
-          <Card className='h-[320px] p-0 flex flex-col'>
-            <CardHeader
-              title='Beyond the Code'
-              description='Explore my interest and hobbies beyond the code.'
-              className='px-6  py-6'
-            />
-            <div className='relative flex-1'>
-              {hobbies.map((item) => {
-                return (
-                  <div
-                    key={item.title}
-                    className='inline-flex gap-2 px-6 bg-gradient-to-r items-center from-emerald-300 to-sky-400 rounded-full py-1.5 absolute'
-                    style={{
-                      left: item.left,
-                      top: item.top,
-                    }}
-                  >
-                    <span className='font-medium text-gray-950'>
-                      {item.title}
-                    </span>
-                    <span>{item.emoji}</span>
-                  </div>
-                );
-              })}
-            </div>
-          </Card>
-
-          <Card className='h-[320px] p-0 relative'>
-            <Image
-              src={mapImage}
-              alt='Book Cover'
-              width={600}
-              height={600}
-              className='h-full w-full object-cover'
-            />
-            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full  bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[""] after:absolute after:inset-0  after:outline-2 after:outline after:-outline-offset-2 after:rounded-full after:outline-white after:pointer-events-none'>
-              <Image
-                src={smileMemoji}
-                alt='Smile Memoji'
-                width={500}
-                height={500}
-                className='size-20'
               />
-            </div>
-          </Card>
+            </Card>
+          </div>
+          <div className='flex md:grid flex-col md:grid-cols-5 gap-8 lg:grid-cols-3 '>
+            <Card className='h-[320px] flex flex-col md:col-span-3 w-[100] lg:col-span-2'>
+              <CardHeader
+                title='Beyond the Code'
+                description='Explore my interest and hobbies beyond the code.'
+                // className=''
+              />
+              <div className='relative flex-1 '>
+                {hobbies.map((item) => {
+                  return (
+                    <div
+                      key={item.title}
+                      className='inline-flex gap-2 px-6  bg-gradient-to-r items-center from-emerald-300 to-sky-400 rounded-full py-1.5 absolute'
+                      style={{
+                        left: item.left,
+                        top: item.top,
+                      }}
+                    >
+                      <span className='font-medium text-gray-950'>
+                        {item.title}
+                      </span>
+                      <span>{item.emoji}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </Card>
+
+            <Card className='h-[320px] p-0 relative md:col-span-2 lg:col-span-1'>
+              <Image
+                src={mapImage}
+                alt='Book Cover'
+                width={600}
+                height={600}
+                className='h-full w-full object-cover object-left-top'
+              />
+              <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full  bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[""] after:absolute after:inset-0  after:outline-2 after:outline after:-outline-offset-2 after:rounded-full after:outline-white after:pointer-events-none'>
+                <Image
+                  src={smileMemoji}
+                  alt='Smile Memoji'
+                  width={500}
+                  height={500}
+                  className='size-20'
+                />
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
