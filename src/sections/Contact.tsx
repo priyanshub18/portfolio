bb38ef9f54d089bc193237d77182fbb530f1080b
priyanshub18 <priyanshubhardwaj658@gmail.com>
2025-02-17 11:51:03 +0530

    added-footer

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -4,13 +4,13 @@ export const ContactSection = () => {
   return (
     <div className='py-16 pt-12'>
       <div className='container'>
-        <div className='flex flex-col items-center justify-center md:flex-row gap-5 bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-10 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0'>
+        <div className='flex flex-col items-center justify-between md:flex-row gap-5 bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-10 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0'>
           <div
             className='absolute inset-0 opacity-5 -z-10'
             style={{ backgroundImage: `url(${grainImage.src})` }}
           ></div>
 
-          <div>
+          <div className='max-w-2xl'>
             <h2 className='font-serif text-2xl md:text-3xl'>
               Let's create something amazing together
             </h2>
@@ -20,8 +20,9 @@ export const ContactSection = () => {
             </p>
           </div>
           <div>
-            <button className='text-white bg-black rounded-xl px-6 py-2 mt-[6px] h-12 inline-flex items-center justify-center gap-2 w-max-content'>
-              <span>Contact Me</span>
+            <button className='text-white bg-black rounded-xl px-6 py-2 mt-[6px] h-12 inline-flex items-center justify-center gap-2'>
+              <span className='hidden md:block lg:hidden'>Contact</span>
+              <span className='block md:hidden lg:block'>Contact Me</span>
               <ArrowUpRightIcon className='size-4' />
             </button>
           </div>
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,59 @@
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+const footerLinks = [
+  {
+    title: 'Github',
+    link: 'https://github.com/priyanshu-bhardwaj',
+  },
+  {
+    title: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/priyanshu-bhardwaj/',
+  },
+  {
+    title: 'Instagram',
+    link: 'https://www.instagram.com/priyanshu_bhardwaj/',
+  },
+  {
+    title: 'Twitter',
+    link: 'https://twitter.com/priyanshu_bhardwaj',
+  },
+  {
+    title: 'LeetCode',
+    link: 'https://leetcode.com/priyanshu_bhardwaj/',
+  },
+];
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className='relative -z-10 overflow-x-clip '>
+      <div
+        className='absolute h-[600px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30
+       [mask-image:radial-gradient(50%_50%_at_bottom_center , black , transparent )] -z-10'
+        style={{
+          WebkitMaskImage:
+            'radial-gradient(50% 50% at bottom center, black, transparent)',
+          maskImage:
+            'radial-gradient(50% 50% at bottom center, black, transparent)',
+        }}
+      ></div>
+      <div className='container'>
+        <div className='border-t border-white/20 py-6 text-sm flex flex-col justify-between md:flex-row items-center gap-8'>
+          <div className='text-white/40'>&copy; 2023 Priyanshu Bhardwaj</div>
+          <nav className='flex flex-col md:flex-row items-center gap-8'>
+            {footerLinks.map((link) => {
+              return (
+                <a
+                  key={link.title}
+                  href={link.link}
+                  target='_blank'
+                  className='inline-flex items-center gap-1.5'
+                >
+                  <span className='font-semibold'>{link.title}</span>
+                  <ArrowUpRightIcon className='size-4' />
+                </a>
+              );
+            })}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
